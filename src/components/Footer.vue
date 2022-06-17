5274d98a656a3875e5ecdd1566051b4e842703d1
810505339@qq.com <810505339@qq.com>
2022-06-17 15:52:47 +0800

    修改git url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
     <a
       icon-btn i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
+      href="http://github.com/810505339/a-player-music"
       target="_blank"
       title="GitHub"
     />
